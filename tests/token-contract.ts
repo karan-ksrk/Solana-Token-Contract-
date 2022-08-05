2c8c8d927c1bc32c9abd0450a494a21c26ddd4e0
karan-ksrk <karanksrk2000@outlook.com>
2022-08-05 22:16:00 +0530

    added smartcontract for mint and transfer token

---
+++ tests/token-contract.ts
@@ -0,0 +1,15 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { TokenContract } from "../target/types/token_contract";
+
+describe("token-contract", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.TokenContract as Program<TokenContract>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    console.log("Your transaction signature");
+  });
+});
